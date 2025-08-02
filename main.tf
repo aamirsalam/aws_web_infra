0977821c70c11b7be0d346bd324c48956f7a6694
Aamir Salam <aamir.salam@khoros.com>
2025-08-03 02:51:38 +0530

    Initial commit: Add clean Terraform code

---
+++ main.tf
@@ -0,0 +1,160 @@
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    name = "Production VPC"
+  }
+}
+
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  tags = {
+    Name = "prod-gw"
+  }
+}
+
+resource "aws_egress_only_internet_gateway" "egress_ipv6" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
+
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.egress_ipv6.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+
+
+resource "aws_subnet" "prod-subnet" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Prod subnet"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.prod-subnet.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+
+#security group
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = aws_vpc.prod-vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+#   security_group_id = aws_security_group.allow_web.id
+#   cidr_ipv6         = aws_vpc.prod-vpc.ipv6_cidr_block
+#   from_port         = 80
+#   ip_protocol       = "tcp"
+#   to_port           = 80
+# }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ssh" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"   # Or restrict to your IP for better security
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+#Network Interface
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.prod-subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+#Elastic IP. Has a depends on flag with IGW; set depends_on flag with IGW.
+
+resource "aws_eip" "one" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [ aws_internet_gateway.gw ]
+}
+
+#Create EC2 instance
+
+resource "aws_instance" "my_first_server" {
+  ami           = "ami-09e6f87a47903347c"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "aamir_ec2"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install httpd -y
+              systemctl start httpd
+              systemctl enable httpd
+              sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+              EOF
+
+  tags = {
+    name = "web-server"
+  }
+
+}
