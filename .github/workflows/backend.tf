cdf6755c24752b11e84f9688d67753e1cb155aa7
Aamir Salam <aamir.salam5@gmail.com>
2025-09-13 18:14:18 +0530

    Remove  backend

--- .github/workflows/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstate-aamir-demo"
-    key            = "aws_web_infra/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_locks"
-    encrypt        = true
-  }
-}
